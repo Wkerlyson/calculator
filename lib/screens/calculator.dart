661a72ed748a43431722c4767b6fa3f4076577ff
Wkerlyson <wkerlyson.batista@gmail.com>
2020-02-10 23:35:07 -0300

    adding action to the buttons

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -3,13 +3,17 @@ import 'package:calculator/components/keyboard.dart';
 import 'package:flutter/material.dart';
 
 class Calculator extends StatelessWidget {
+  _onPressed(String text) {
+    print(text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Column(
         children: <Widget>[
           Display('123.45'),
-          Keyboard(),
+          Keyboard(_onPressed),
         ],
       ),
     );
