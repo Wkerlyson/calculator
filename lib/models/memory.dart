a22b9835c43a0219610f8097632a2ef5a2ad02f1
Wkerlyson <wkerlyson.batista@gmail.com>
2020-02-11 21:27:24 -0300

    Creating the applyCommand and allClear functions

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,1 +1,19 @@
-class Momory {}
+class Memory {
+  String _value = '0';
+
+  void applyCommand(String command) {
+    if (command == 'AC') {
+      _allClear();
+    } else {
+      _value += command;
+    }
+  }
+
+  _allClear() {
+    _value = '0';
+  }
+
+  String get value {
+    return _value;
+  }
+}
